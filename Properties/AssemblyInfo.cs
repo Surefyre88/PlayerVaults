2563025126d725230173014aaa0176c34b8ac765
Surefyre88 <chris@arnoldnetwork.com>
2020-01-25 00:58:54 -0600

    ItemVault Plugin

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ItemVault")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ItemVault")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Sven Mawby")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fb37e036-0806-42f5-b451-f786689a9066")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
